e2ba8459be7c47eb0eb499c6fd16e8a0b8fe2982
GeloUno <geloke59@gmail.com>
2021-10-23 14:56:15 +0200

    use query to get data system

--- src/components/systemData.page.tsx
+++ src/components/systemData.page.tsx
@@ -1,5 +1,26 @@
+import { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+import { getSystemData, ErrorSystem } from '../controllers/getSystemData';
+import { SystemData } from '../models/systemData';
+
+//
+
 function SystemDataPage() {
-  return <div>SystemDataPage</div>;
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+  }: {
+    data: AxiosResponse<SystemData> | undefined;
+    isLoading: boolean;
+    isError: boolean;
+    error: ErrorSystem | null;
+  } = useQuery('systemData', getSystemData, {
+    refetchInterval: 10000,
+  });
+
+  return <div></div>;
 }
 
 export default SystemDataPage;
