07cd0b7d645f32f54a55f6cb0474b2d467674547
GeloUno <geloke59@gmail.com>
2021-10-23 18:13:55 +0200

    use spinner

--- src/components/systemData.page.tsx
+++ src/components/systemData.page.tsx
@@ -4,7 +4,8 @@ import { getSystemData, ErrorSystem } from '../controllers/getSystemData';
 import { SystemData } from '../models/systemData';
 import ChartView from './chartView';
 import { nanoid } from 'nanoid';
-
+import ScaleLoader from 'react-spinners/ScaleLoader';
+import { getColorType } from '../shared/colors';
 //
 
 function SystemDataPage() {
@@ -12,18 +13,34 @@ function SystemDataPage() {
     data,
     isLoading,
     isError,
-    error,
-  }: {
+  }: // error,
+  {
     data: AxiosResponse<SystemData> | undefined;
     isLoading: boolean;
     isError: boolean;
     error: ErrorSystem | null;
   } = useQuery('systemData', getSystemData, {
-    refetchInterval: 10000,
+    // refetchInterval: 20000,
   });
+  console.log(
+    '<- LOG -> file: systemData.page.tsx -> line 25 -> SystemDataPage -> data',
+    data
+  );
 
   if (isLoading) {
-    return <div>loading</div>;
+    return (
+      <div className="flex flex-col">
+        <ScaleLoader color={getColorType({ typeData: 'Errors' })} margin={5} />
+        <ScaleLoader
+          color={getColorType({ typeData: 'Warnings' })}
+          margin={5}
+        />
+        <ScaleLoader
+          color={getColorType({ typeData: 'Operations' })}
+          margin={5}
+        />
+      </div>
+    );
   }
   if (isError || data === undefined) {
     return <div>error</div>;
@@ -31,6 +48,10 @@ function SystemDataPage() {
 
   return (
     <div>
+      <div className="py-4">
+        <div className={' text-3xl uppercase '}>{data.data.Name}</div>
+        <p className={' text-sm py-2 opacity-50'}>{data.data.Url}</p>
+      </div>
       <ChartView key={nanoid()} data={data.data.Errors} typeData="Errors" />
       <ChartView
         key={nanoid()}
