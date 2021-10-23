559a787c28130ed7a409bc89fb383907bc844d9e
GeloUno <geloke59@gmail.com>
2021-10-23 14:54:24 +0200

    Add get system data and class Error System

--- src/controllers/getSystemData.ts
+++ src/controllers/getSystemData.ts
@@ -1,6 +1,34 @@
 import axios from "axios";
-import { URL } from "../shared/const";
+import { URL } from "../shared/constants";
+import { SystemData } from '../models/systemData';
+import { AxiosResponse } from 'axios';
 
-export function getSystemData() {
-    return axios.get(URL)
+// export function getSystemData() {
+//     return axios.get(URL)
+// }
+
+export class ErrorSystem extends Error {
+    code: string;
+    constructor(code: string, message: string) {
+        super(message);
+        this.code = code
+    }
+
+}
+
+export async function getSystemData(): Promise<SystemData> {
+    try {
+        const data: AxiosResponse<SystemData> = await axios.get(URL)
+        return data.data
+
+    } catch (error: unknown) {
+
+        if (axios.isAxiosError(error)) {
+            if (error.code) {
+                throw new ErrorSystem(error.code, error.message)
+            }
+            throw new ErrorSystem('404', error.message)
+        }
+        throw new ErrorSystem('404', "error fetch data")
+    }
 }
